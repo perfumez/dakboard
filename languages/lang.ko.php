5bd471833c20e197fe685c2676b66c21315f9d64
perfumez <perfumez@hotmail.com>
2017-12-13 21:22:25 +0900

    Create lang.ko.php

---
+++ languages/lang.ko.php
@@ -0,0 +1,56 @@
+<?php
+/* 
+------------------
+Language: Korean
+------------------
+*/
+$lang = array();
+// Days of the week
+$lang['monday'] = '월요일';
+$lang['monday_abbr'] = '월';
+$lang['tuesday'] = '화요일';
+$lang['tuesday_abbr'] = '화';
+$lang['wednesday'] = '수요일';
+$lang['wednesday_abbr'] = '수';
+$lang['thursday'] = '목요일';
+$lang['thursday_abbr'] = '목';
+$lang['friday'] = '금요일';
+$lang['friday_abbr'] = '금';
+$lang['saturday'] = '토요일';
+$lang['saturday_abbr'] = '토';
+$lang['sunday'] = '일요일';
+$lang['sunday_abbr'] = '일';
+// Months of the year
+$lang['january'] = '1월';
+$lang['january_abbr'] = '';
+$lang['february'] = '2월';
+$lang['february_abbr'] = '';
+$lang['march'] = '3월';
+$lang['march_abbr'] = '';
+$lang['april'] = '4월';
+$lang['april_abbr'] = '';
+$lang['may'] = '5월';
+$lang['may_abbr'] = '';
+$lang['june'] = '6월';
+$lang['june_abbr'] = '';
+$lang['july'] = '7월';
+$lang['july_abbr'] = '';
+$lang['august'] = '8월';
+$lang['august_abbr'] = '';
+$lang['september'] = '9월';
+$lang['september_abbr'] = '';
+$lang['october'] = '10월';
+$lang['october_abbr'] = '';
+$lang['november'] = '11월';
+$lang['november_abbr'] = '';
+$lang['december'] = '12월';
+$lang['december_abbr'] = '';
+// Other terms used
+$lang['today'] = '오늘';
+$lang['tomorrow'] = '내일';
+$lang['all_day'] = '하루종일';
+$lang['calendar'] = '캘린더';
+$lang['weather'] = '날씨';
+$lang['todo'] = 'To-do';
+$lang['loading'] = 'Loading';
+?>
